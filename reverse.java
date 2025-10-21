0c64eefcebc25abf5f0541afb6d48bac992b127b
Mayank Mali <121378811+malimayank@users.noreply.github.com>
2025-10-21 17:09:40 +0530

    Add program to reverse a string in Java

---
+++ reverse.java
@@ -0,0 +1,7 @@
+class Main {
+  public static void main(String[] args) {
+    String s = "HelloWorld";
+    String rev = new StringBuilder(s).reverse().toString();
+    System.out.println(rev);
+  }
+}
